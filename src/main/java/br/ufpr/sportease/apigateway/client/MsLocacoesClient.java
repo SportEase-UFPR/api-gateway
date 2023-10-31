dc299a979c2900bda78bc1432a2fc06065cf76be
Rafael Dias Simeoni <rafaelsimeoni28@gmail.com>
2023-10-30 22:15:34 -0300

    teste chamada ms-locacoes

--- src/main/java/br/ufpr/sportease/apigateway/client/MsLocacoesClient.java
+++ src/main/java/br/ufpr/sportease/apigateway/client/MsLocacoesClient.java
@@ -44,7 +44,7 @@ public class MsLocacoesClient {
 
     public Object verificarHorariosDisponiveisParaLocacao(Object request) {
         //String url = urlMsLocacoes + "/horarios-disponiveis";
-        String url = "https://api-gateway-production-75aa.up.railway.app/locacoes/horarios-disponiveis";
+        String url = "https://ms-locacoes-production.up.railway.app/locacoes/horarios-disponiveis";
         HttpHeaders headers = gerarCabecalho();
         return restTemplate.exchange(url, HttpMethod.POST, new HttpEntity<>(request, headers), Object.class).getBody();
     }
