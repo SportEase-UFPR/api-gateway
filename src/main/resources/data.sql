8d7569972c73ab3c714289d5aee033b817d22f9f
Rafael Dias Simeoni <rafaelsimeoni28@gmail.com>
2023-11-27 20:59:29 -0300

    insert inicial do adm

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,8 @@
-INSERT IGNORE INTO credenciais.tb_usuarios (id, ativa, login, nivel_acesso, senha, bloqueada) VALUES (1, true ,'adm@gmail.com', 'ROLE_ADM', '$2a$10$vavo6GdtwWFNy6aJQueJM.uXCPjsIwXu4KkwnmzT6Zu8Oa4VClIIm', false);
+INSERT INTO credenciais.tb_usuarios (id, ativa, login, nivel_acesso, senha, bloqueada)
+SELECT 1, true, 'adm@gmail.com', 'ROLE_ADM', '$2a$10$vavo6GdtwWFNy6aJQueJM.uXCPjsIwXu4KkwnmzT6Zu8Oa4VClIIm', false
+FROM dual
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM credenciais.tb_usuarios
+    WHERE id = 1
+);
